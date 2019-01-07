c6abaa6bf01b8569f2c6767cd13b850dc054d36f
ayoubkachkach <ayoub.kachkach@gmail.com>
2019-01-07 22:37:38 +0100

    Changed asgmt 3 part B to latest working version

--- ass03_part2.hs
+++ ass03_part2.hs
@@ -1,5 +1,6 @@
 type Binding = [(Name, LExpr)]
-type Evaluator = LExpr -> Binding -> Bool -> LExpr
+type Evaluator = LExpr -> Binding -> LExpr
+type Executor = LExpr -> Binding -> LExpr -> Binding
 
 type Name = String
 data LExpr =  V Name
@@ -19,11 +20,25 @@ data LExpr =  V Name
             | LET (Name, LExpr) LExpr
             deriving (Eq, Show)
 
+
 errorMsg :: Maybe LExpr -> String -> String
 errorMsg (Just a) extraMsg = "Error evaluating node (" ++ show a ++ ")." ++ extraMsg
-errorMsg Nothing extraMsg = "Error. ++ extraMsg
+errorMsg Nothing extraMsg = "Error." ++ extraMsg
 
-evalB ::
+evalB :: Evaluator
+evalB (B b) _ = (B b)
+evalB expr _  = error (errorMsg (Just expr) " Expected bool literal.")
+
+evalI :: Evaluator
+evalI (I i) _ = (I i)
+evalI expr _  = error (errorMsg (Just expr) " Expected int literal.")
+
+evalV :: Evaluator
+evalV (V v) bindings = 
+    case lookup v bindings of
+      Just val -> eval val bindings  -- found variable, lazy eval
+      Nothing  -> error (errorMsg (Just (V v)) " Unbound variable.") -- undefined variable
+
 extractOp :: LExpr -> Maybe (String, LExpr, LExpr)
 extractOp expr =
   case expr of
@@ -38,7 +53,32 @@ extractOp expr =
 isOp :: LExpr -> Bool 
 isOp expr =
   case extractOp expr of
-    Nothing = False
-    Just _  = True
+    Nothing -> False
+    Just _  -> True
 
-reduce 
+iOp :: (Integer -> Integer -> Integer) -> LExpr -> LExpr -> LExpr
+iOp op (I a) (I b) = (I (op a b))
+iOp _ _ _          = error (errorMsg Nothing " Couldn't resolve binary operation. Expected integers.")
+
+bOp :: (Bool -> Bool -> Bool) -> LExpr -> LExpr -> LExpr
+bOp op (B a) (B b) = (B (op a b))
+bOp _ _ _          = error (errorMsg Nothing " Couldn't resolve binary operation. Expected booleans.")
+
+evalOp :: Evaluator
+evalOp expr bindings =
+  let 
+    opStrToOp  = [("Add", (iOp (+))), ("Sub", (iOp (-))), ("Mult", (iOp (*))), ("Div", (iOp (div))), ("And", (bOp (&&))), ("Or", (bOp (||)))]
+  in
+    case extractOp expr of
+      Nothing             -> error (errorMsg (Just expr) " Expected binary operation.")
+      Just (opStr, a, b)  -> let Just op = lookup opStr opStrToOp 
+                                 x = eval a bindings
+                                 y = eval b bindings
+                             in 
+                                 op x y
+
+eval :: Evaluator
+eval (I i) _            = (I i)
+eval (B b) _            = (B b)
+eval (V v) b            = evalV (V v) b
+eval expr b | isOp expr = evalOp expr b 
